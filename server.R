95f23580e72497fa889139b437955188873311e5
Alexandre de Souza <alexandre@aledsz.com.br>
2018-12-17 13:47:10 -0200

    Pequeno ajuste no server.R

--- server.R
+++ server.R
@@ -1,14 +1,10 @@
 shinyServer(function(input, output, globar) {
   
-  Playlists <- observeEvent(eventExpr = input$searchButton,
-                            handlerExpr = get_user_playlists(input$user))
-  
-  names_playlist <- observe(if(nrow(Playlists) > 0){
-    Playlists$playlist_name
+  Playlists <- eventReactive(input$searchButton, {
+    get_user_playlists(input$user)
   })
   
-  output$selected_var <- renderText({ 
-    names_playlist
+  output$selected_var <- renderText({
+    Playlists()$playlist_name
   })
-  ###FIM####
-  })
+})
